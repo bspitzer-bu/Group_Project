fe762d25e4bc12d60ce93d5c9b6c8fca36ca1401
Amanda Wakefield <awake@bu.edu>
2019-02-26 14:46:44 -0500

    removed duplicate home url

--- src/config/urls.py
+++ src/config/urls.py
@@ -25,7 +25,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomeView.as_view(), name='home'),
-    path('', views.HomeView.as_view(), name='home'),
     path('contact/', views.ContactView, name='contact'),
     path('usage/', views.UsageView, name='usage')
 ]
