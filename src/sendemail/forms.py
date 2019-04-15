b3d01e28abf9e087d0d16c151ba03bc08b876b8c
schifferl <schifferl@users.noreply.github.com>
2019-04-15 10:27:38 -0400

    finish contact form styling

--- src/sendemail/forms.py
+++ src/sendemail/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    sender = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    sender = forms.EmailField(required = True)
+    subject = forms.CharField(required = True)
+    message = forms.CharField(required = True, widget = forms.Textarea)
+
+    sender.widget.attrs.update({'class': 'form-control', 'placeholder': 'Name'})
+    subject.widget.attrs.update({'class': 'form-control', 'placeholder': 'Email'})
+    message.widget.attrs.update({'class': 'form-control', 'placeholder': 'Message'})
