1a5cfc8dd248c06af8922602211f39538160a07f
schifferl <schifferl@users.noreply.github.com>
2019-04-21 22:50:54 -0400

    add additional R scripts for db update

---
+++ binds.R
@@ -0,0 +1,19 @@
+library(magrittr)
+
+pdbids <-
+  base::list.files("src/media/fastas") %>%
+  stringr::str_remove_all(".fasta") %>%
+  stringr::str_c(collapse = ",")
+
+base::paste0("https://www.rcsb.org/pdb/rest/customReport.csv") %>%
+  base::paste0("?pdbids=", pdbids) %>%
+  base::paste0("&reportName=Ligands") %>%
+  base::paste0("&service=wsfile") %>%
+  base::paste0("&format=csv") %>%
+  readr::read_csv() %>%
+  dplyr::rename_all(snakecase::to_snake_case) %>%
+  dplyr::filter(!base::is.na(ligand_id)) %>%
+  dplyr::select(structure_id, ligand_id) %>%
+  dplyr::rename(pdb_id = structure_id) %>%
+  dplyr::distinct() %>%
+  readr::write_csv("binds.csv")
---
+++ ligand.R
@@ -0,0 +1,23 @@
+library(magrittr)
+
+pdbids <-
+  base::list.files("src/media/fastas") %>%
+  stringr::str_remove_all(".fasta") %>%
+  stringr::str_c(collapse = ",")
+
+base::paste0("https://www.rcsb.org/pdb/rest/customReport.csv") %>%
+  base::paste0("?pdbids=", pdbids) %>%
+  base::paste0("&reportName=Ligands") %>%
+  base::paste0("&service=wsfile") %>%
+  base::paste0("&format=csv") %>%
+  readr::read_csv() %>%
+  dplyr::rename_all(snakecase::to_snake_case) %>%
+  dplyr::filter(!base::is.na(ligand_id)) %>%
+  dplyr::select(ligand_id, ligand_name, ligand_formula, ligand_molecular_weight, in_ch_i_key) %>%
+  dplyr::rename(name = ligand_name) %>%
+  dplyr::rename(formula = ligand_formula) %>%
+  dplyr::rename(molecular_weight = ligand_molecular_weight) %>%
+  dplyr::rename(inchi_key = in_ch_i_key) %>%
+  dplyr::mutate_if(base::is.character, stringr::str_to_upper) %>%
+  dplyr::distinct() %>%
+  readr::write_csv("ligand.csv")
