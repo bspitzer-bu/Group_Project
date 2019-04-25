9e4b7e1f8901999acda25068020f7b9ff296aeb2
schifferl <schifferl@users.noreply.github.com>
2019-04-24 21:55:54 -0400

    remove duplicates from similarities table

--- r_scripts/similarities.R
+++ r_scripts/similarities.R
@@ -1,11 +1,16 @@
 library(magrittr)
 
-readr::read_csv("rmsd.csv") %>%
+rmsd_matrix <-
+  readr::read_csv("cvs_files/rmsd.csv") %>%
   tibble::column_to_rownames(var = "X1") %>%
-  base::as.matrix() %>%
-  dfrtopics::gather_matrix() %>%
+  base::as.matrix()
+
+rmsd_matrix[lower.tri(rmsd_matrix, diag = TRUE)] <- NA
+
+dfrtopics::gather_matrix(rmsd_matrix) %>%
   magrittr::set_colnames(base::c("gpcr1", "gpcr2", "rmsd")) %>%
   dplyr::mutate(id = NA_character_) %>%
   dplyr::mutate(sequence = NA_character_) %>%
   dplyr::select(id, gpcr1, gpcr2, rmsd, sequence) %>%
-  readr::write_csv("similarities.csv", na = "")
+  dplyr::filter(!is.na(rmsd)) %>%
+  readr::write_csv("cvs_files/similarities.csv", na = "")
